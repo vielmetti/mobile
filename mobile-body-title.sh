40400bb87ca69e9b3fd68b5e534745f7f3d0e3f9
Edward Vielmetti <edward.vielmetti@gmail.com>
2013-10-11 19:32:54 -0400

    minor format tweaks

--- mobile-body-title.sh
+++ mobile-body-title.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo '<!-- body title -->'
 echo '<li data-theme="c">'
-echo '<h2 href="#'$1'" data-transition="slide"> '
+echo '<a href="#'$1'" data-transition="slide"> '
 echo $1
-echo '</h2>'
+echo '</a>'
